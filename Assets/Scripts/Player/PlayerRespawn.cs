725448a2605f3b9c1b568836a2b435b9a68ddaf2
WillasT <wrorrong@student.ciputra.ac.id>
2025-10-21 11:03:02 +0700

    both players now respawn

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -3,15 +3,40 @@ using UnityEngine;
 public class PlayerRespawn : MonoBehaviour
 {
     // [SerializeField] private AudioClip checkpointSound;
-    private Transform currentCheckpoint;
+    
+    // We NO LONGER need a private 'currentCheckpoint' here.
+    // The manager will handle it.
+    
     private Animator anim;
+    private Vector3 startPosition; // Each player still has its own start position
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        startPosition = transform.position;
+    }
 
     public void Respawn()
     {
-        transform.position = currentCheckpoint.position;
+        // --- THIS IS THE FIX ---
+        // Ask the CheckpointManager where to go
+        if (CheckpointManager.instance.currentCheckpoint != null)
+        {
+            // If the manager has a checkpoint, go there
+            transform.position = CheckpointManager.instance.currentCheckpoint.position;
+        }
+        else
+        {
+            // If not, go back to our own personal start position
+            transform.position = startPosition;
+        }
+        // --- END OF FIX ---
 
-        anim.ResetTrigger("die");
-        anim.Play("Idle");
+        if (anim != null)
+        {
+            anim.ResetTrigger("die");
+            anim.Play("Idle");
+        }
     }
 
     // Activate checkpoints 
@@ -19,9 +44,12 @@ public class PlayerRespawn : MonoBehaviour
     {
         if (collision.transform.tag == "Checkpoint")
         {
-            currentCheckpoint = collision.transform;
+            // --- THIS IS THE FIX ---
+            // Tell the manager that we just hit a new checkpoint
+            CheckpointManager.instance.SetCurrentCheckpoint(collision.transform);
+            
             // SoundManager.instance.PlaySound(checkpointSound); 
-            collision.GetComponent<Collider2D>().enabled = false; // Deatviate checkpoint collider 
+            collision.GetComponent<Collider2D>().enabled = false; // Deactivate checkpoint collider 
         } 
     }
 }
