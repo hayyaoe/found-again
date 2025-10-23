c3f2bda3f892f626e6f2fa663db51263ba6ab8eb
derendmarvel <derend101@gmail.com>
2025-10-23 09:30:34 +0700

    feat: added pause to sound effects and bgm

--- Assets/Scripts/Utils/SoundFXManager.cs
+++ Assets/Scripts/Utils/SoundFXManager.cs
@@ -1,59 +1,82 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager instance;
+
     [SerializeField] private AudioSource soundFXObject;
+    private List<AudioSource> activeAudioSources = new List<AudioSource>();
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
     {
-        // Spawn in GameObject
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
-
-        // Assign the AudioClip
         audioSource.clip = audioClip;
-
-        // Assign Volume
         audioSource.volume = volume;
-
-        // Play sound
         audioSource.Play();
 
-        // Get length of sound FX clip
-        float clipLength = audioSource.clip.length;
+        activeAudioSources.Add(audioSource);
 
-        // Destroy after done playing
+        float clipLength = audioSource.clip.length;
         Destroy(audioSource.gameObject, clipLength);
+        StartCoroutine(RemoveFromListAfterDelay(audioSource, clipLength));
     }
 
-    public void PlayRandomSoundFXClip(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    public void PlayRandomSoundFXClip(AudioClip[] audioClips, Transform spawnTransform, float volume)
     {
-        // Assign a random index
-        int rand = Random.Range(0, audioClip.Length);
-
-        // Spawn in GameObject
+        int rand = Random.Range(0, audioClips.Length);
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
-
-        // Assign the AudioClip
-        audioSource.clip = audioClip[rand];
-
-        // Assign Volume
+        audioSource.clip = audioClips[rand];
         audioSource.volume = volume;
-
-        // Play sound
         audioSource.Play();
 
-        // Get length of sound FX clip
-        float clipLength = audioSource.clip.length;
+        activeAudioSources.Add(audioSource);
 
-        // Destroy after done playing
+        float clipLength = audioSource.clip.length;
         Destroy(audioSource.gameObject, clipLength);
+        StartCoroutine(RemoveFromListAfterDelay(audioSource, clipLength));
+    }
+
+    // ✅ Pause all current sound effects
+    public void PauseAllSoundFX()
+    {
+        foreach (AudioSource source in activeAudioSources)
+        {
+            if (source != null && source.isPlaying)
+            {
+                source.Pause();
+            }
+        }
+    }
+
+    // ✅ Resume all paused sound effects
+    public void ResumeAllSoundFX()
+    {
+        foreach (AudioSource source in activeAudioSources)
+        {
+            if (source != null && !source.isPlaying)
+            {
+                source.UnPause();
+            }
+        }
+    }
+
+    // ✅ Clean up list after sounds are destroyed
+    private System.Collections.IEnumerator RemoveFromListAfterDelay(AudioSource source, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        activeAudioSources.Remove(source);
     }
 }
